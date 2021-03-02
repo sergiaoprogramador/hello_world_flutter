9a07ff4b8587fdc1530547fa30db93080b46ee98
Sergio Ramos <sergio.ramos@enesolucoes.com.br>
2021-03-02 18:01:37 -0400

    introduce Material APP

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,12 @@ class AppWidget extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          title,
-          textDirection: TextDirection.ltr,
-          style: TextStyle(color: Colors.white, fontSize: 50.0)
-        ) 
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.red
+      ),
+      home: Container(
+        child: Center(child: Text("Sergião")),
       ),
     );
   }
